2e9debf1e9435c801c80a18281386ee633a75736
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2022-10-31 09:49:08 +0000

    Revert "correction erreurs cell_lec_de_bits + commentaires"
    This reverts commit 88354d49b117335ef49c09a559e7b511294b7422

--- cell_lecture_de_bit.s
+++ cell_lecture_de_bit.s
@@ -21,10 +21,9 @@ sw $ra 0($sp)
 sw $a0 4($sp) #bit dont on veut connaitre la valeur
 sw $a1 8($sp) #n
 #corps
-addi $a0 $a0 -1 # position du n-ieme bit -> $a0
-srlv $t0 $a1 $a0 # obtenir le n-ieme bit en premier bit de poids faible-> $t0
+srlv $t0 $a0 $a1 
 li $t1 1
-and $v0 $t0 $t1 # mettre tous les bits a 0 sauf le bit recherché
+and $v0 $t0 $t1 
 #epilogue
 lw $ra 0($sp)
 lw $a0 4($sp)
